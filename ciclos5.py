924c222ac0cbaadc2f63b384e54d6f39e0cb5239
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-21 15:35:02 -0600

    ciclos 5

---
+++ ciclos5.py
@@ -0,0 +1,17 @@
+"""
+ciclo for en python
+
+Programa que pida 10 números e imprima el promedio de estos.
+
+Se utilizan los conceptos del programa anterior de contador y acumulador.
+
+estructura:
+Do While contador < 10
+    Console.Write("Ingrese un número: ")
+    num = CDbl(Console.ReadLine())
+    suma = suma + num
+    contador = contador + 1
+Loop
+
+Dim promedio As Double
+promedio = suma / 10
