d71a22f6a54b25b22bfcefca42706ee4ec25f2f7
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-21 17:15:57 -0600

    ciclos9

---
+++ ciclos9.py
@@ -0,0 +1,21 @@
+"""
+ciclos for en python
+
+Escribe un programa que pida el limite inferior y superior de un intervalo. 
+Si el límite inferior es mayor que el superior lo tiene que volver a pedir.
+A continuación se van introduciendo números hasta que introduzcamos el 0. 
+Cuando termine el programa dará las siguientes informaciones:
+	* La suma de los números que están dentro del intervalo (intervalo abierto).
+	* Cuantos números están fuera del intervalo.
+	* He informa si hemos introducido algún número igual a los límites del intervalo
+"""
+// Inicio del programa y declaración de variables:
+public class Ej05Limites 
+  public static void main(String[10] args)
+    Scanner scanner = new Scanner(System.in);
+    int inferior;
+    int superior;
+    int numero;
+    int suma = 0;
+    int contador = 0;
+    boolean esLimite = false;
