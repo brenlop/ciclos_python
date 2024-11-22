91845ba2389fd0042bc76ce9dee93ec8ecfb13ee
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-21 18:43:25 -0600

    ciclos 15

---
+++ ciclos15.py
@@ -0,0 +1,27 @@
+"""
+ciclos for python
+Realizar un programa que pida al usuario un número entero y muestre el mismo número en binario
+"""
+switch (centenas)
+case 0:
+texto = "";
+ break;
+ case 1:
+    texto = "Ciento";
+break;
+case 2:
+texto = "Doscientos";
+ break;
+ case 3:
+texto = "Trecientos";
+break;
+case 4:
+ texto = "Cautrocientos";
+ break;
+case 5:
+texto = "Quinientos";
+ break;
+case 6:
+ texto = "Seiscientos";
+ break;
+            
