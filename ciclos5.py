5ff0263ef0907cd22d1a6245aca4df4df64a1a0d
brenlop <brenlop2708@gmail.com>
2024-12-13 12:56:57 -0600

    ciclos5.py

--- ciclos5.py
+++ ciclos5.py
@@ -1,16 +1,20 @@
-"""
-ciclo for en python
-
+'''
 Programa que pida 10 números e imprima el promedio de estos.
 
 Se utilizan los conceptos del programa anterior de contador y acumulador.
+'''
+print("Calculadora de Promedio - Intorduce 10 números")
+print("-" * 20)
 
-estructura:
-Do While contador < 10
-    Console.Write("Ingrese un número: ")
-    num = CDbl(Console.ReadLine())
-    suma = suma + num
-    contador = contador + 1
-Loop
-Dim promedio As Double
+numeros = []  
+suma = 0
+for i in range(10):
+    numero = float(input(f"Inroduce el número {i+1}: "))
+    numeros.append(numero)
+    suma += numero
 promedio = suma / 10
+print("\nResultados:")
+print("-" * 30)
+print(f"Los números introducuidos son: {numeros}")
+print(f"La suma total de los numeros es: {suma}")
+print(f"El promedio es: {promedio}")
