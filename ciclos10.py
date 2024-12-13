a4ecd01924b340a65e5219513e4efe0993b2d529
brenlop <brenlop2708@gmail.com>
2024-12-13 12:41:57 -0600

    ciclos10.py

--- ciclos10.py
+++ ciclos10.py
@@ -1,13 +1,24 @@
-"""
-ciclos for python
- Escribe un programa que dados dos números, uno real (base) y un entero positivo 
+''
+Escribe un programa que dados dos números, uno real (base) y un entero positivo 
 (exponente), saque por pantalla el resultado de la potencia. No se puede 
 utilizar el operador de potencia.
-"""
- base=float(input("Base:"))
- exp=int(input("Exponente:"))
- potencia=1
- for cont in range(1,exp+1):
-     potencia=potencia*base
- print("Potencia=%f"%potencia)
+'''
+while True:
+    try:
+        base = float(input("Introduce la base: "))
+        break
+    except ValueError:
+        print("Error: Por favor ingrese un número válido")
+while True:
+    try:
+        exponente = int(input("Ingrese el exponente ( numero entero positivo): "))
+        if exponente < 0:
+            print("Error: El exponente tiene que ser positivo")
+        break
+    except ValueError:
+        print("Error: Por favor introduzca un número entero")
 
+resultado = 1
+for i in range(exponente):
+    resultado *= base
+print(f"{base} elevado a la {exponente} es: {resultado}")
