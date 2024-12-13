aad45f19484c8c9b73292d439096c3ede5920cd5
brenlop <brenlop2708@gmail.com>
2024-12-13 12:44:34 -0600

    Update ciclos1.py

--- ciclos1.py
+++ ciclos1.py
@@ -1,25 +1,17 @@
-"""
-ciclo for en python
-
- un número y calcule su factorial (El factorial de 
+'''
+Crea una programa que pida un número y calcule su factorial (El factorial de 
 un número es el producto de todos los enteros entre 1 y el propio número y se 
 representa por el número seguido de un signo de exclamación. 
 Por ejemplo 5! = 1x2x3x4x5=120)
-"""
+'''
+n = int(input("igrese el numero: "))
+factorial = 1
+if n < 0:
+    print("Error el factorial no se realiza en numeros negativos")
+elif n == 0:
+    print("el fa torial de 0 es 1")
 
-estructura:
-#include <stdio.h>
-
-int main() {
-    int num;
-    long factorial = 1;
-
-    printf("Ingrese un número: ");
-    scanf("%d", &num);
-
-    if (num < 0)
-     { printf("Error: El número debe ser positivo.\n");
-     return 1;}
-    for (int i = 1; i <= num; i++) 
-    {factorial *= i;}
-
+else:
+ for i in range(1, n +1):
+    factorial *= i
+    print(f"el factorial de {n} es {factorial} ")
