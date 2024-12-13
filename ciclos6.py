dc3c904e5e45282329693665edad84c0ce72392b
brenlop <brenlop2708@gmail.com>
2024-12-13 12:57:41 -0600

    ciclos6.py

--- ciclos6.py
+++ ciclos6.py
@@ -1,41 +1,22 @@
-"""
-ciclo for en python
-
+'''
 Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de 
 números a introducir). El programa debe informar de cuantos números introducidos 
 son mayores que 0, menores que 0 e iguales a 0.
-"""
- Inicializo los contadores
-2.- Leer cantidad de números
-3.- Desde 1 hasta cantidad de números 		
-4.- Leer num
-5.- Si num> 0-> incremento cont_positivos
-6.- Si num< 0-> incremento cont_negativos
-7.- Si num = 0-> incremento cont_ceros.
-Muestro cont_postivos, cont_negativos, cont_cero
-Proceso ContarNumerosPositivosNegativosYCeros
-Definir num Como Entero;
-Definir cantidad_num,i Como Entero;
-Definir cont_negativos,cont_positivos,cont_ceros Como Entero;
-cont_negativos <- 0;
-cont_positivos <- 0;
-cont_ceros <- 0;
-Escribir "¿Cuántos números vas a introducir?:";
-Leer cantidad_num;
-Para i<-1 Hasta cantidad_num Hacer
-Escribir "Número ",i,":" ;
-Leer num;
-Si num>0 Entonces
-cont_positivos <- cont_positivos + 1;
-Sino 
-Si num<0 Entonces
-cont_negativos <- cont_negativos + 1;
-SiNo
-cont_ceros <- cont_ceros + 1;
-FinSi
-FinSi
-FinPara
-Escribir "Números positivos:",cont_positivos;
-Escribir "Números negativos:",cont_negativos;
-Escribir "Números igual a 0:",cont_ceros;
-FinProceso
+'''
+print("Calculadora de Promedio al ingresar 10 números")
+print("-" * 20)
+
+numeros = [] 
+suma = 0
+for i in range(10):
+    numero = float(input(f"Intorduzca el número {i+1}: "))
+    numeros.append(numero)
+    suma += numero
+promedio = suma / 10
+
+#Resultados
+print("\nResultados:")
+print("-" * 30)
+print(f"Los números intorducidos son: {numeros}")
+print(f"La suma total de los numeros son: {suma}")
+print(f"El promedio es: {promedio}")
