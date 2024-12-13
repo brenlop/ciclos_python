20bf17bf886ed09b3ccca4de09dc431998e387fb
brenlop <brenlop2708@gmail.com>
2024-12-13 12:51:08 -0600

    ciclos12.py

--- ciclos12.py
+++ ciclos12.py
@@ -1,15 +1,11 @@
-"""
-ciclos for python 
+'''
 Realizar un programa para determinar cuánto ahorrará una persona en un año, 
 si al final de cada mes deposita cantidades variables de dinero; 
 además, se quiere saber cuánto lleva ahorrado cada mes.
-"""
-estructura:
-Console.WriteLine("Ingrese la cantidad de dinero que depositará cada mes:")
-
-For i As Integer = 1 To meses
-    Console.Write("Mes " & i & ": ")
-    ahorradoMensual = CDbl(Console.ReadLine($1000))
-    ahorradoAnual += ahorradoMensual
-    Console.WriteLine("Ahorrado hasta el mes " & i & ": " & ahorradoAnual & vbCrLf)
-
+'''
+print("Medidor de ahorros")
+total = 0
+for i in range(1,13):
+    ah = float(input(f"Coloca la cantidad del mes {i}: "))
+    total = ah + total
+    print(f"llevas {total} ahorrados")
