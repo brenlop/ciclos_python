de2ce9695959457ed38a084b35b93e8870afe544
brenlop <brenlop2708@gmail.com>
2024-12-13 12:55:36 -0600

    ciclos3.py

--- ciclos3.py
+++ ciclos3.py
@@ -1,39 +1,56 @@
-"""
-
-ciclos for en python
-
-Algoritmo que pida números hasta que se introduzca un cero. Debe imprimir la suma
+'''
+Programa que pida números hasta que se introduzca un cero. Debe imprimir la suma
 y la media de todos los números introducidos.
+
 Para este problema se requiere de un acumulador y un contador
+
 Contador: Variable que va, como su nombre lo indica, contando elementos (iteraciones),
 por cada iteración el contador va incrementando en 1
-"""
-#include <iostream
-using namespace std;
+Ejemplo:
+contador = 0
+for i in range(5):
+    contador = contador + 1
+print(contador)
 
-int main(int argc, char *argv[]) {
-	int num;
-	int suma,cont;
-	float media;
-	suma = 0;
-	cont = 0;
-	//Con el mientras si el primer número es 0 no va a entrar en el bucle
-	cout << "Número (0 para salir):";
-	cin >> num;
-	while(num!=0) {
-		suma = suma + num;
-		cont = cont + 1;
-		cout << "Número (0 para salir):";
-		cin >> num;
-	}
-	//Si cont=0 no puedo realizar la división
-	if(cont>0) {
-		media = float(suma) / cont;
-	}
-	else
-	{
-		media =0;
-	}
-	cout << "Suma:" << suma << endl;
-	cout << "Media:" << media;
-	return 0;
+Acumulador: Variable que va, como su nombre lo indica, acumulando valores en cada iteración,
+por cada iteración al valor inicial se le suman nuevos valores al acumulador
+Ejemplo:
+acumulador = 0;
+for i in range(5):
+    acumulador = acumulador + i
+print(acumulador)
+
+'''
+print("Este programa calcula la suma y la media de todos los numeros ingresados, cuando se ingresa un 0 termina el programa")
+
+
+suma = 0
+contador = 0
+ningresados = []
+while True:
+    try:
+        entrada = input("Igresa un número: ")
+        
+        # Verificar si la entrada contiene solo dígitos y punto decimal
+        if not all(c.isdigit() or c == '.' or (c == '-' and entrada.index(c) == 0) for c in entrada):
+            print("Error: Solo se permiten números")
+            continue    
+        numero = float(entrada)
+        
+        if numero == 0:
+            break 
+        # Actualizar suma y contador y almacenamiento de numeros 
+        suma += numero
+        contador += 1
+        ningresados.append(numero)
+    except ValueError:  
+        print("Error: Entrada no válida. Por favor, ingresa un número válido")
+
+if contador > 0:
+    media = suma / contador
+    print("\nResultados:")
+    print(f"\nLa suma total de todos los numeros ingresados es: {suma}")
+    print(f"La media es: {media}")
+    print(f"Los numeros ingresados son: {ningresados} ")
+else:
+    print("\nNo se introdujeron números")
